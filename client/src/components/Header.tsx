b29b5ac3d071b6e832e14428eb0be468f1592b19
Bryan Chalker <bchalker@users.noreply.github.com>
2026-06-12 16:57:31 -0400

    Deploy production updates

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -26,6 +26,25 @@ const MARKETING_SECTIONS = [
 const MARKETING_MOBILE_NAV_BODY_CLASS = 'app-left-nav--mobile-open-body'
 const MARKETING_MOBILE_NAV_MQ = '(min-width: 761px)'
 
+const EXPECTIFI_LOGO_SRC = '/branding/expectifi-logo.svg'
+const EXPECTIFI_LOGO_MOBILE_SRC = '/branding/expectifi-logo-mobile.svg'
+
+function HeaderBrandLogo() {
+  return (
+    <picture className="header__logo-picture">
+      <source media="(max-width: 760px)" srcSet={EXPECTIFI_LOGO_MOBILE_SRC} />
+      <img
+        className="header__logo"
+        src={EXPECTIFI_LOGO_SRC}
+        alt=""
+        width={170}
+        height={53}
+        decoding="async"
+      />
+    </picture>
+  )
+}
+
 type HeaderAuthProps = {
   onSignIn: () => void
   onCreateAccount: () => void
@@ -74,7 +93,7 @@ function HeaderBrand({
   if (onboardingMode) {
     return (
       <div className="header__brand header__brand--onboarding" aria-label="Expectifi">
-        <span className="header__mark">Expectifi</span>
+        <HeaderBrandLogo />
       </div>
     )
   }
@@ -101,7 +120,7 @@ function HeaderBrand({
 
   return (
     <button type="button" className="header__brand" onClick={handleClick} aria-label={ariaLabel}>
-      <span className="header__mark">Expectifi</span>
+      <HeaderBrandLogo />
     </button>
   )
 }
